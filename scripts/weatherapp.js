1abd364ee0ecf003129c46dd1a55447678f23d35
Brice187 <lars@kerick.net>
2016-06-08 22:47:08 +0200

    Added toggle button for celsius / fahrenheit. Still need to tweak design :)

--- scripts/weatherapp.js
+++ scripts/weatherapp.js
@@ -1,19 +1,32 @@
+var unit = "metric";
+
 function showPosition(position) {
     'use strict';
     document.getElementById("data").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+function changeUnit() {
+    if (unit === "metric") {
+        unit = "imperial";
+         document.getElementById("changeunit").innerHTML = "Fahrenheit";
+    } else {
+        unit = "metric";
+        document.getElementById("changeunit").innerHTML = "Celsius";
+    }
+    getLocation();
 }
 
 function getWeatherForLocation(position) {
     'use strict';
     var lat = parseFloat(position.coords.latitude).toFixed(2);
     var lon = parseFloat(position.coords.longitude).toFixed(2);
-    var json = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=metric&appid=5ea8cbbb99b8ef0874beab4865254419";
+    var json = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + unit + "&appid=5ea8cbbb99b8ef0874beab4865254419";
 
-    $.getJSON(json, function (a) {
+    $.getJSON(json, function(a) {
 
-        document.getElementById("data").innerHTML = "Temperatur: "+ a.main.temp + " celsius degree<br>Condition: "+ a.weather[0].main + "<br>Description: " + a.weather[0].description + "<br>City: " + a.name;
+        document.getElementById("data").innerHTML = "Temperatur: " + a.main.temp + " " + unit + " degree<br>Condition: " + a.weather[0].main + "<br>Description: " + a.weather[0].description + "<br>City: " + a.name;
 
-         });
+    });
 }
 
 function getLocation() {
