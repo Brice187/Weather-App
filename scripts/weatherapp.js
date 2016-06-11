231dc68292e1f215fa4b2193f6d6565292847ebd
Brice187 <lars@kerick.net>
2016-06-11 08:52:17 +0200

    bugfix for last commit

--- scripts/weatherapp.js
+++ scripts/weatherapp.js
@@ -1,50 +1,52 @@
 var unit = "metric";
 
 function showPosition(position) {
-    'use strict';
-    document.getElementById("data").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+	'use strict';
+	document.getElementById("data").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
 }
 
 function changeUnit() {
-    if (unit === "metric") {
-        unit = "imperial";
-        document.getElementById("changeunit").innerHTML = "Fahrenheit";
-    } else {
-        unit = "metric";
-        document.getElementById("changeunit").innerHTML = "Celsius";
-    }
-    getLocation();
+	'use strict';
+	if (unit === "metric") {
+		unit = "imperial";
+		document.getElementById("changeunit").innerHTML = "Fahrenheit";
+	} else {
+		unit = "metric";
+		document.getElementById("changeunit").innerHTML = "Celsius";
+	}
+	getLocation();
 }
 
 function getWeatherForLocation(position) {
-    'use strict';
-    var lat = parseFloat(position.coords.latitude).toFixed(2);
-    var lon = parseFloat(position.coords.longitude).toFixed(2);
-    var json = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + unit + "&appid=5ea8cbbb99b8ef0874beab4865254419";
+	'use strict';
+	var lat = parseFloat(position.coords.latitude).toFixed(2);
+	var lon = parseFloat(position.coords.longitude).toFixed(2);
+	var json = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + unit + "&appid=5ea8cbbb99b8ef0874beab4865254419";
 
-    $.getJSON(json, function(a) {
+	$.getJSON(json, function (a) {
 
-        document.getElementById("data").innerHTML = "Temperatur: " + a.main.temp + " " + unit + " degree<br>Condition: " + a.weather[0].main + "<br>Description: " + a.weather[0].description + "<br>City: " + a.name;
-        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
-                tags: a.weather[0].main,
-                tagmode: "any",
-                format: "json"
-            },
-            function(data) {
-                $.each(data.items, function(i, item) {
-                    $("<img />").attr("src", item.media.m).replaceAll("#images");
-                    if (i == 0) return false;
-                });
-            });
-
-    });
+		document.getElementById("data").innerHTML = "Temperatur: " + a.main.temp + " " + unit + " degree<br>Condition: " + a.weather[0].main + "<br>Description: " + a.weather[0].description + "<br>City: " + a.name;
+		$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+				tags: a.weather[0].main
+				, tagmode: "any"
+				, format: "json"
+			}
+			, function (data) {
+				$.each(data.items, function (i, item) {
+					$("<img />").attr("src", item.media.m).replaceAll("#images");
+					if (i === 0) {
+						return false
+					};
+				});
+			});
+	});
 }
 
 function getLocation() {
-    'use strict';
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getWeatherForLocation);
-    } else {
-        document.getElementById("data").innerHTML = "Geolocation is not supported by this browser.";
-    }
+	'use strict';
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(getWeatherForLocation);
+	} else {
+		document.getElementById("data").innerHTML = "Geolocation is not supported by this browser.";
+	}
 }
