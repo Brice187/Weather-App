5ce43f5a463781273e5c8562eafd4b303b2c0c63
Brice187 <lars@kerick.net>
2016-06-17 20:46:11 +0200

    Added all required user stories challenges. Still some design stuff to do :)

--- scripts/weatherapp.js
+++ scripts/weatherapp.js
@@ -9,10 +9,10 @@ function changeUnit() {
 	'use strict';
 	if (unit === "metric") {
 		unit = "imperial";
-		document.getElementById("changeunit").innerHTML = "Fahrenheit";
+		document.getElementById("changeunit").innerHTML = "Imperial system";
 	} else {
 		unit = "metric";
-		document.getElementById("changeunit").innerHTML = "Celsius";
+		document.getElementById("changeunit").innerHTML = "Metric system";
 	}
 	getLocation();
 }
@@ -24,10 +24,15 @@ function getWeatherForLocation(position) {
 	var json = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=" + unit + "&appid=5ea8cbbb99b8ef0874beab4865254419";
 
 	$.getJSON(json, function (a) {
-
-		document.getElementById("data").innerHTML = "Temperatur: " + a.main.temp + " " + unit + " degree<br>Condition: " + a.weather[0].main + "<br>Description: " + a.weather[0].description + "<br>City: " + a.name;
+		document.getElementById("data").innerHTML =
+			"Location: " + a.name + ", " + a.sys.country +
+			"<br>Temperatur: " + a.main.temp + " " + getTempSystem(unit) +
+			" degree<br>Condition: " + a.weather[0].main +
+			"<br>Atmospheric pressure: " + a.main.pressure +
+			"<br>Humidity: " + a.main.humidity +
+			"<br>Wind Speed: " + a.wind.speed + " " + getDistSystem(unit);
 		$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
-				tags: a.weather[0].main
+				tags: a.weather[0].description
 				, tagmode: "any"
 				, format: "json"
 			}
@@ -35,12 +40,25 @@ function getWeatherForLocation(position) {
 				$.each(data.items, function (i, item) {
 					$("<img />").attr("src", item.media.m).replaceAll("#images");
 					if (i === 0) {
-						return false
+						return false;
 					};
 				});
 			});
 	});
 }
+function getTempSystem(t) {
+	if(t==="metric")
+		return "celsius";
+	else
+		return "fahrenheit";
+}
+function getDistSystem(d) {
+	if(d==="metric")
+		return "meter/sec";
+	else
+		return "miles/hour";
+}
+
 
 function getLocation() {
 	'use strict';
